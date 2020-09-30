ff689999f7ffc089d219d650477978ec4ee752c8
HirotoMaezato <maezatohiroto1031@gmail.com>
2020-10-01 04:35:55 +0900

    ライフサイクルのやつ

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LikeButton from './LikeButton';
 
 const Article = (props) => {
     
@@ -7,7 +8,8 @@ const Article = (props) => {
         <div>
             <h2>{props.title}</h2>
             <label htmlFor="check">公開状態</label>
-            <input type="checkbox" checked={props.isPublished} id="check"/>
+            <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()}/>
+            <LikeButton count = {props.count}/>
         </div>
     )
 }
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -6,8 +6,38 @@ class Blog extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            isPublished: false
+            isPublished: false,
+            count: 0
         }
+    }
+
+    componentDidMount() {
+        document.getElementById('counter').addEventListener('click', this.countUp)
+    }
+
+    componentDidUpdate() {
+        if (this.state.count >= 10) {
+            this.setState({
+                count: 0
+            })
+        }
+    }
+
+    componentWillUnmount() {
+        document.getElementById('counter').removeEventListener('click', this.countUp)
+    }
+
+    togglePublished = () => {
+        this.setState({
+            isPublished: !this.state.isPublished
+        })
+    }
+
+    
+    countUp = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
     }
 
     render() {
@@ -16,6 +46,11 @@ class Blog extends React.Component {
             <>
                 <Article    title = {"React,Redux"}
                             isPublished = {this.state.isPublished}
+                            // stateで定義したデータをpropsの形で渡している
+                            toggle = {() => this.togglePublished()}
+                            // ↑関数をpropsでArticle.jsxに渡している
+                            // 関数の渡し方があるから注意しよう
+                            count = {this.state.count}
                             
                 />
             </>
