b3b6ca977a93935ff107f0e57336a879feb596d7
HirotoMaezato <maezatohiroto1031@gmail.com>
2020-10-01 07:17:25 +0900

    react hocksを使ってみた

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,61 +1,44 @@
 import React from 'react';
 import Article from './Article';
+import * as FooBar from './components/FooBar';
+import Hoge from './components/Hoge';
 
-
-class Blog extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isPublished: false,
-            count: 0
-        }
-    }
-
-    componentDidMount() {
-        document.getElementById('counter').addEventListener('click', this.countUp)
-    }
-
-    componentDidUpdate() {
-        if (this.state.count >= 10) {
-            this.setState({
-                count: 0
-            })
-        }
-    }
-
-    componentWillUnmount() {
-        document.getElementById('counter').removeEventListener('click', this.countUp)
-    }
-
-    togglePublished = () => {
-        this.setState({
-            isPublished: !this.state.isPublished
-        })
-    }
-
+const Blog = () => {
     
-    countUp = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
+    // componentDidMount() {
+    //     document.getElementById('counter').addEventListener('click', this.countUp)
+    // }
 
-    render() {
-        const authorName = 'HirotoMaezato';
-        return(
-            <>
-                <Article    title = {"React,Redux"}
-                            isPublished = {this.state.isPublished}
-                            // stateで定義したデータをpropsの形で渡している
-                            toggle = {() => this.togglePublished()}
-                            // ↑関数をpropsでArticle.jsxに渡している
-                            // 関数の渡し方があるから注意しよう
-                            count = {this.state.count}
-                            
-                />
-            </>
-        );
-    }
+    // componentDidUpdate() {
+    //     if (this.state.count >= 10) {
+    //         this.setState({
+    //             count: 0
+    //         })
+    //     }
+    // }
+
+    // componentWillUnmount() {
+    //     document.getElementById('counter').removeEventListener('click', this.countUp)
+    // }
+
+  
+    // countUp = () => {
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     })
+    // }
+
+
+    return(
+        <>
+            <Article    title = {"React,Redux"}
+                        // count = {this.state.count}       
+            />
+            <FooBar.Foo/>
+            <FooBar.Bar/>
+            <Hoge/>
+        </>
+    );
 }
 
 export default Blog;
